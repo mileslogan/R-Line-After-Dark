0e90a7a2e142ea871ae9eb32bfd62e1154dc6e53
Miles Logan <mlr582@nyu.edu>
2021-05-09 17:23:01 -0400

    project setting stuff

--- RLineAfterDark/Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ RLineAfterDark/Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/21/2021 10:04:19 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/9/2021 5:02:17 PM";}}
